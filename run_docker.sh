3868036bc0dff94dd2e41be2c8f872740981d2f6
msjithin <msjithin108@gmail.com>
2024-04-08 21:17:09 -0700

    moves key for encoding/decoing tokens to txt file

--- run_docker.sh
+++ run_docker.sh
@@ -2,7 +2,7 @@
 docker build -t myapi .
 
 # run the app on docker
-docker run -d -p 8080:80 myapi
+docker run -v ./secret_key_file:/app -d -p 8080:80 myapi
 
 # compose docker so it reloads 
 docker-compose up --build 
